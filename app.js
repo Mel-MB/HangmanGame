e7976ac206557d36d665e6e95a47bf13f6b0da09
Mel-MB <71283318+Mel-MB@users.noreply.github.com>
2021-08-06 16:04:45 +0200

    letters display

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+//UI Elements
+const wordEl = document.getElementById('word');
+
+
+// Application variables
 const words = ['application', 'programmation', 'interface', 'developpeur', 'javascript'];
-
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-
 let playable = true;
 
 const correctLetters = [];
 const wrongLetters = [];
 
-// Keydown letter press evvent
+// Keydown letter press Event
 window.addEventListener('keydown', e => {
 	if (playable) {
 		if (e.key >= 65 && e.key <= 122) {
@@ -17,8 +20,10 @@ window.addEventListener('keydown', e => {
 				if (!correctLetters.includes(letter)) {
 					correctLetters.push(letter);
 
-					// displayWord();
+					displayWord();
+
 				} else {
+
 					// showNotification();
 				}
 			} else {
@@ -26,7 +31,9 @@ window.addEventListener('keydown', e => {
 					wrongLetters.push(letter);
 
 					// updateWrongLettersDisplay();
+
 				} else {
+
 					// showNotification();
 				}
 			}
@@ -34,3 +41,25 @@ window.addEventListener('keydown', e => {
 	}
 });
 
+// Show hidden / correct letters for the word
+function displayWord() {
+	wordEl.innerHTML = `
+    ${selectedWord
+			.split('')
+			.map(
+				letter => `<span class="letter">
+                    ${correctLetters.includes(letter) ? letter : ''}
+                </span>`
+			)
+			.join('')}
+  `;
+
+	const writtenWord = wordEl.innerText.replace(/\n/g, '');
+
+	if (writtenWord === selectedWord) {
+		finalMessage.innerText = 'Félicitations! Vous avez gagné! 🥳';
+		popup.style.display = 'flex';
+
+		playable = false;
+	}
+}
